9920733861a73abcb805753bde664f488c226147
Juan Martin <103914139+juanmaxx2@users.noreply.github.com>
2022-05-08 14:35:40 -0300

    Add files via upload

--- ejercicio 6/Main.py
+++ ejercicio 6/Main.py
@@ -1,5 +1,4 @@
 import csv
-from errno import ELIBBAD
 from Viaje import Viajero
 
 if __name__ == '__main__':
@@ -13,37 +12,26 @@ if __name__ == '__main__':
         lista.append(objeto)
     numero = int(input('\ningrese numero de pasajero:'))
     opcion = None
+    otroviajero=Viajero('Francisco','Manzano','34932432','5','2454')
     for objeto in lista:
-        if numero == objeto.numero():
+        if numero == objeto.getNumero():
             while opcion!='d':
                 print('\n')
-                print ('a) Cantidad de millas')
+                print ('a) Comparar millas')
                 print ('b) Acumular millas')
                 print ('c) Canjear millas')
-                print ('d) el viajero que tiene mas millas')
-                print ('e) restar millas')
-                print ('f) sumar millas')
-                print ('g) salir')
+                print ('d) Salir')
                 opcion = input('\nIngrese opcion a realizar:')
+ 
+                if opcion=='a':
+                    for  i in range(len(lista)):
+                        if objeto.getMillas()==otroviajero.getMillas():    
+                            print('\nEl vaijero frecuente tiene menos millas que el viajero:', format(lista[i].getNombre()))
+                        else: print('\nEl vaijero frecuente no tiene menos millas que el viajero:', format(lista[i].getNombre()))
                 
-                if opcion == 'a':
-                    cantidad=objeto.CantidadTotalMillas()
-                    print ('\ncantidad total de millas acumuladas es:', format(cantidad))
-                  
-                elif opcion == 'b':
-                    milla=int(input('\ncantidad de millas que recorridas:'))
-                    acum=objeto.AcumularMillas(milla)
-                    print('\nla cantidad de millas en total es:', format(acum))
-                      
-                elif opcion == 'c':
-                    cantmi=int(input('\ningrese la cantidad de millas que desesa canjear:'))
-                    cant=objeto.CanjearMillas(acum,cantmi)
-                  
-                elif opcion=='d':
-                    objeto.__gt__(lista)
+                elif opcion=='b':
+                    print('\nLas millas acumuladas son:', objeto.getMillas()+otroviajero.getMillas())
                 
-                elif opcion=='e':
-                    print('la cantidad de millas acumuldas son:',objeto.__add__(lista))
-                
-                elif opcion=='f':
-                    print('la cantidad de millas restadas es:',lista.__sub__(lista))
+                elif opcion=='c':
+                    x=int(input('\nIngrese el valor que quiere canjear:'))
+                    print('\nLas millas canjeadas son:', otroviajero.getMillas()-x)
--- ejercicio 6/Viaje.py
+++ ejercicio 6/Viaje.py
@@ -1,6 +1,3 @@
-from ast import If
-
-
 class Viajero():
     __Nombre=""
     __Apellido=""
@@ -9,55 +6,29 @@ class Viajero():
     __millas_acum=int
     
     def __init__(self,nombre,apellido,dni,num_viajero,millas_acum):
-        self.__nombre=nombre
-        self.__apellido=apellido
-        self.__dni=dni
+        self.__Nombre=nombre
+        self.__Apellido=apellido
+        self.__DNI=dni
         self.__num_viajero=num_viajero
         self.__millas_acum=millas_acum
     
+    def getNombre(self):
+        return self.__Nombre
+    
     def getMillas(self):
-        return self.__millas_acum
+        return int(self.__millas_acum)
+    
+    def getNumero(self):
+        return self.__num_viajero
     
     def __str__(self):
         return str(self.__num_viajero, self.__millas_acum)
     
-    def numero(self):
-        return self.__num_viajero
+    def __gt__(self,other):
+        return self.getMillas()<other.getMillas()
     
-    def CantidadTotalMillas(self):
-        return (self.__millas_acum)
+    def __add__(self,other):
+        return self.getMillas()+other.getMillas()
     
-    def AcumularMillas(self, millas=int):
-        i=self.__millas_acum+millas
-        return i
-    
-    def CanjearMillas(self, acumuladas, canjearmilla=int):
-        if (self.__millas_acum>=canjearmilla):
-            print ('\nse canjearon las millas')
-            acumuladas=acumuladas-canjearmilla
-            return (acumuladas)
-        else: 
-            print ('\nno se canjearon')
-            return (acumuladas)
-
-    def __gt__(self,lista):
-        vf=lista[0]
-        i=1
-        for i in range(len(lista)):
-            if int(self.__millas_acum)>int(vf.getMillas):
-                return ('el viajero tiene mas millas acumuladas que el viajero')
-            else: return ('el viajero no tiene mas millas acumuladas')
-
-    def __add__(self,lista):
-        vf=lista[0]
-        i=1
-        for i in range(len(lista)):
-            vf.getMillas += lista.getMilla
-        return (vf)
-
-    def __sub__(self,lista):
-        vf=lista[0]
-        i=1
-        for i in range(len(lista)):
-            vf.getMillas -= lista.getMilla
-        return (vf)
+    def __sub__(self,other):
+        return self.getMillas()-other
